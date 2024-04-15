076a558455476d59381b4a5ccb91fe04ac4d9273
Arnab Mitra <arnabmitra471@gmail.com>
2024-04-15 21:03:59 +0530

    Learnt about variables and data types

---
+++ src/Variables.kt
@@ -0,0 +1,44 @@
+fun main() {
+    var firstName = "Arnab"
+    firstName = "John"
+
+    var age = 22
+
+    val maxIntegerValue:Int = Int.MAX_VALUE
+
+    val minIntegerValue:Int = Int.MIN_VALUE
+    println("The max integer value is $maxIntegerValue")
+    println("The min integer value is $minIntegerValue")
+
+    val minFloatValue = Float.MIN_VALUE
+    val maxFloatValue = Float.MAX_VALUE
+
+    println("The min float value is $minFloatValue")
+    println("The max float value is $maxFloatValue")
+
+    val minDoubleValue = Double.MIN_VALUE
+    val maxDoubleValue = Double.MAX_VALUE
+    println("The min double value is $minDoubleValue")
+    println("The max double value is $maxDoubleValue")
+
+    val num:Long = 23458797890
+    println("The long value is $num")
+
+    val myMaxByteValue = Byte.MAX_VALUE
+    val myMinByteValue = Byte.MIN_VALUE
+    println("The max byte value is $myMaxByteValue")
+    println("The min byte value is $myMinByteValue")
+
+    val myMinShort = Short.MIN_VALUE
+    val myMaxShort = Short.MAX_VALUE
+    println("The min short value is $myMinShort")
+    println("The max short value is $myMaxShort")
+
+    val myMinLong = Long.MIN_VALUE
+    val myMaxLong = Long.MAX_VALUE
+    println("The min long value is $myMinLong")
+    println("The max long value is $myMaxLong")
+
+    println("Hello $firstName your age is $age !")
+
+}
