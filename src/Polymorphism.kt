0a2442da576804029e0909fb985adb456df5c04e
Arnab Mitra <arnabmitra471@gmail.com>
2024-04-20 11:05:45 +0530

    Learnt about abstract classes and their features

--- src/Polymorphism.kt
+++ src/Polymorphism.kt
@@ -2,12 +2,20 @@ fun main(){
     val shapes = arrayOf(Circle(9.0),Circle(5.0),Square(5.0),Square(15.0),Triangle(4.0,5.0))
     calculateAreas(shapes)
 
-    val generalShape = Shape()
-    printArea(generalShape)
-
-    val square = Square(4.0)
+    val square:Shape = Square( 4.0)
+    square.area()
     printArea(square)
+    square.display()
 
+    val circle:Shape = Circle(20.0)
+    circle.area()
+    printArea(circle)
+    circle.display()
+
+    val triangle:Shape = Triangle(44.0,32.0)
+    triangle.area()
+    printArea(triangle)
+    triangle.display()
 }
 fun calculateAreas(shapes: Array<Shape>){
     for(shape in shapes)
@@ -18,16 +26,19 @@ fun calculateAreas(shapes: Array<Shape>){
 fun printArea(shape:Shape){
     println(shape.area())
 }
-open class Shape
+abstract class Shape
 {
-    open fun area():Double{
-        return 0.0
-    }
+    abstract fun area():Double
+    abstract fun display()
 }
 class Circle(val radius:Double):Shape()
 {
     override fun area():Double{
         return Math.PI * radius * radius
+    }
+
+    override fun display() {
+        println("Circle is getting displayed")
     }
 }
 class Square(val side:Double):Shape()
@@ -35,11 +46,18 @@ class Square(val side:Double):Shape()
     override fun area():Double{
         return side * side
     }
+    override fun display(){
+        println("Square is getting displayed")
+    }
 }
 class Triangle(val base:Double,val height:Double):Shape()
 {
     override fun area():Double
     {
         return 0.5 * base * height
+    }
+
+    override fun display() {
+        println("Triangle is getting displayed")
     }
 }
